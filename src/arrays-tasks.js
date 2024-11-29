85e50f6bac9a9d13cb199df7a686cd5e489f6669
Meruert <mika.amantaikuzu@gmail.com>
2024-11-29 23:34:13 +0500

    feat: done getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -122,8 +122,11 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (!arr.length) return 0;
+
+  const average = arr.reduce((acc, num) => acc + num, 0) / arr.length;
+  return Number.isInteger(average) ? average : +average.toFixed(2);
 }
 
 /**
