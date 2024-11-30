c1758d86d9793cdd469dc2b9f5575e81dd04d957
Meruert <mika.amantaikuzu@gmail.com>
2024-11-30 22:49:00 +0500

    feat: done getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -444,8 +444,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .filter((num) => num % 2 !== 0)
+    .map((el) => numbers.indexOf(el));
 }
 
 /**
